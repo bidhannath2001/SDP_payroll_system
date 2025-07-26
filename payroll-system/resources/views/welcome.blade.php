7bffae077dc2bba12a83cc72bea6a83ad6518b0a
bidhannath2001 <bidhannath2001@gmail.com>
2025-07-26 21:53:32 +0600

    check everything is ok or not

--- payroll-system/resources/views/welcome.blade.php
+++ payroll-system/resources/views/welcome.blade.php
@@ -1,2 +1,7 @@
 <h1>Laravel Project is created successfully!</h1>
 <h1>Database Name: <mark>payroll_system</mark> </h1>
+<h3>Our team members</h3>
+<li>Rafi</li>
+<li>Aritra</li>
+<li>Bidhan</li>
+<li>Sourav</li>
